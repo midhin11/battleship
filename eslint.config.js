029bda5167e9110c90432781f595c3afce71c3f8
midhin11 <midhincr7@gmail.com>
2026-05-22 11:26:55 +0530

    Edit config files

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,12 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], 
     plugins: { js }, 
     extends: ["js/recommended"], 
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+        },
+    },
     rules: {
       "no-unused-vars": "off",
     }
