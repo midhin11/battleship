9f35c98a3ea70a7008be9142411a20beb5d6d19a
midhin11 <midhincr7@gmail.com>
2026-05-24 19:02:21 +0530

    Remove unnecessary selectors

--- src/selectors.js
+++ src/selectors.js
@@ -1,9 +1,3 @@
 //selectors.js
 
 export const gameContainer = document.querySelector(".game-container");
-gameContainer.classList.add("game-container");
-
-export const playerContainer = document.querySelector(".player-container");
-export const compContainer = document.querySelector(".comp-container");
-export const playerBoardDisplay = document.querySelector(".player-board");
-export const compBoardDisplay = document.querySelector(".comp-board");
